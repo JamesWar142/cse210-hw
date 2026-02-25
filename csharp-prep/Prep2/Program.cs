938368cc6354f72cdf76a5d7f35cf85190f9dac7
JamesDWyatt <wya22003@byui.edu>
2026-02-25 10:20:25 -0700

    W01 Assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        string symbol;
+        Console.Write("What is your grade: ");
+        int grade = int.Parse(Console.ReadLine());
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        symbol = "";
+        if (letter != "A" && letter != "F")
+        {
+            int symbolNum = grade % 10;
+            if (symbolNum >= 7)
+            {
+                symbol = "+";
+            }
+            else if (symbolNum < 3)
+            {
+                symbol = "-";
+            }
+        }
+
+        Console.WriteLine($"Your grade is {letter}{symbol}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed the class");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class");
+        }
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        int age;
+        Console.Write("Please input your age: ");
+        age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Your age is: {age}");
+
+        if (age <= 100)
+        {
+            Console.WriteLine("You are cool");
+        }
+        else if (age <= 150)
+        {
+            Console.WriteLine("You are sort of cool");
+        }
     }
 }
