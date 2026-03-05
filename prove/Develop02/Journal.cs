f785eeeab30cb674cd04ea3ca4cd8fb8aa74b177
JamesDWyatt <wya22003@byui.edu>
2026-03-05 09:41:52 -0700

    Menu added to class

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,14 +1,4 @@
 public class Journal
 {
-    public List<Prompt> _entries = new List<Prompt>();
-    public void Date()
-    {
-        DateTime theCurrentTime = DateTime.Now;
-        string dateText = theCurrentTime.ToShortDateString();
-        Console.WriteLine(dateText);
-    }
-    public void ReadFromFile()
-    {
-        
-    }
+    
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,7 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Menu menu = new Menu();
-
-        menu.ProcessMenu();
+        bool done = false;
+        int userResponse;
+        do
+        {
+            userResponse = menu.ProcessMenu();
+            switch (userResponse)
+            {
+                case 1:
+                // Create new journal entry
+                // Add entry to list of entries
+                break;
+                case 2:
+                // Display journal entries
+                break;
+                case 3:
+                // Save journal to file
+                break;
+                case 4:
+                // Load journal from file
+                break;
+                case 5:
+                    done = true;
+                    break;
+            }
+        } while (!done);
     }
 }
