a6ef08beae038f28a9dfdfeae3943ddd02328062
JamesDWyatt <wya22003@byui.edu>
2026-03-12 09:45:30 -0600

    3/12/26 Ref Class Finished

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,11 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Word myword = new Word("Bob");
+        Ref myref = new Ref("John 3:15 ");
 
-        Console.WriteLine(myword.GetWordString());
+        Console.WriteLine(myref.Reference() + myword.GetWordString());
 
         myword.HideWord();
-        Console.WriteLine(myword.GetWordString());
+        Console.WriteLine(myref.Reference() + myword.GetWordString());
 
         Word myWord2 = new Word("Moroni");
 
--- prove/Develop03/Ref.cs
+++ prove/Develop03/Ref.cs
@@ -1,4 +1,12 @@
 class Ref
 {
-    
+    private string _ref;
+    public Ref(string r)
+    {
+        _ref = r;
+    }
+    public string Reference()
+    {
+        return _ref;
+    }
 }
