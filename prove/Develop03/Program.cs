0dac9410568ab59e69ae55e42a90949a145ac16b
JamesDWyatt <wya22003@byui.edu>
2026-03-14 20:32:17 -0600

    3/14/26 finished project

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,19 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Word myword = new Word("Bob");
+        Scripture john3 = new Scripture("For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
         Ref myref = new Ref("John 3:15 ");
 
-        Console.WriteLine(myref.Reference() + myword.GetWordString());
-
-        myword.HideWord();
-        Console.WriteLine(myref.Reference() + myword.GetWordString());
-
-        Word myWord2 = new Word("Moroni");
-
-        Console.WriteLine(myWord2.GetWordString());
-
-        myWord2.HideWord();
-        Console.WriteLine(myWord2.GetWordString());
+       while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Press enter to hide words or type quit to end the practice.");
+            Console.WriteLine();
+            Console.WriteLine(myref.Reference() + john3.GetScripture());
+            string input = Console.ReadLine();
+            if (input != null && input == "quit")
+                break;
+            if (john3.AllHidden())
+                {
+                    break;
+                }
+            john3.HideWords(3);
+        }
     }
 }
