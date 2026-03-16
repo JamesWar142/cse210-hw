3b1fb0c040e76e1ff54297a388997623a85f737d
JamesDWyatt <wya22003@byui.edu>
2026-03-16 10:00:14 -0600

    3/16/26 learning parent classes

---
+++ sandbox/PersonProject/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Person person = new Person("Bob", "Ross", 55, 195);
+
+        Console.WriteLine(person.GetPersonInformation());
+
+        Doctor doctor = new Doctor("Ax", "Bob", "Budge", 75, 210);
+
+        Console.WriteLine(doctor.GetDoctorInformation());
+    }
+}
