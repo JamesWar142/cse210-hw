a9a8f565cc2aa24c959a19d7850592717f5141b2
JamesDWyatt <wya22003@byui.edu>
2026-03-16 16:01:02 -0600

    3/16/26 Learning Inheritance Completion

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,24 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName=studentName;
+        _topic=topic;
+    }
+
+    public string StudentName()
+    {
+        return _studentName;
+    }
+    public string Topic()
+    {
+        return _topic;
+    }
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("James Wyatt", "Science");
+        Math math = new Math("James Wyatt", "Math", "7.3", "8-19");
+        Writing writing = new Writing("James Wyatt", "English", "Random Words");
+
+
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
