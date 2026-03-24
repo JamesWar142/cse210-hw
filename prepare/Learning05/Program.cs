dc510a1c815644c0ca1a9b1d00f8a05a9be7f1dc
JamesDWyatt <wya22003@byui.edu>
2026-03-23 20:58:48 -0600

    3/23/26 Learning4 Activity Complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes= new List<Shape>();
+
+
+        Square square = new Square("Yellow", 5);
+        Rectangle rectangle = new Rectangle("Red", 7, 3);
+        Circle circle = new Circle("Blue", 5);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetColor()} shape has an area of {shape.GetArea()}");
+        }
     }
 }
