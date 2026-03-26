69dca21afa90bba96c629aa57aad712fb938976c
JamesDWyatt <wya22003@byui.edu>
2026-03-26 09:30:54 -0600

    3/26/26 finish goal

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -19,8 +19,18 @@ class Goal
         Console.Write("Please enter the name of your goal: ");
         _name = Console.ReadLine();
     }
+    public void SetDescription()
+    {
+        Console.Write("Please enter the description of your goal: ");
+        _description = Console.ReadLine();
+    }
+    public void SetPoints()
+    {
+        Console.Write("Please enter the points of your goal: ");
+        _numberOfPoints = int.Parse(Console.ReadLine());
+    }
     public virtual string GetConsoleString()
     {
-        return $"Goal Information: {_name}";
+        return $"Goal Information: {_name}, {_description}, {_numberOfPoints}";
     }
 }
