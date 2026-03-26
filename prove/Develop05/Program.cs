f517d9b58d578e70d67bd09b25b2a78e445a9813
JamesDWyatt <wya22003@byui.edu>
2026-03-26 10:03:00 -0600

    3/26/26 Menu created

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,15 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Goal myGoal = new Goal();
-        // myGoal.SetName();
-        // myGoal.SetDescription();
-        // myGoal.SetPoints();
-        // Console.WriteLine(myGoal.GetConsoleString());
-
+        bool complete = false;
         SimpleGoal mySimpleGoal = new SimpleGoal();
 
-        mySimpleGoal.CreateGoal();
-        Console.WriteLine(mySimpleGoal.GetConsoleString());
+        Console.WriteLine("Welcome to your Goal Tracker!");
+        while (complete == false)
+        {
+            Console.WriteLine("1. View Goals");
+            Console.WriteLine("2. Create a goal");
+            Console.WriteLine("3. Complete a goal");
+            Console.WriteLine("4. View Total Points");
+            Console.WriteLine("5. Quit");
+            Console.Write("Please select an option: ");
+            int userChoice = int.Parse(Console.ReadLine());
+
+            if (userChoice == 1)
+            {
+                Console.WriteLine(mySimpleGoal.GetConsoleString());
+            }
+            else if(userChoice == 2)
+            {
+                mySimpleGoal.CreateGoal();
+            }
+            else if(userChoice == 3)
+            {
+                
+            }
+            else if(userChoice == 4)
+            {
+                
+            }
+            else if(userChoice == 5)
+            {
+                Console.WriteLine("Goodluck with your goals!");
+                complete = true;
+            }
+            else
+            {
+                Console.WriteLine("Please enter a proper choice.");
+            }
+        }
     }
 }
