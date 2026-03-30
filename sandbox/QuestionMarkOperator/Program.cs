48c1c0d014f7c429e1170d8a3220254b8d2abc09
JamesDWyatt <wya22003@byui.edu>
2026-03-30 09:47:41 -0600

    3/30/26 QuestionMarkOperator Complete

--- sandbox/QuestionMarkOperator/Program.cs
+++ sandbox/QuestionMarkOperator/Program.cs
@@ -2,6 +2,39 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hey Bob");
+        // Console.WriteLine("Hey Bob");
+
+        // int x = 10;
+
+        // string name = (x > 10) ? "bob" : "Betty";
+
+        // Console.WriteLine(name);
+
+        // if (x > 10)
+        // {
+        //     name = "bob";
+        // }
+        // else
+        // {
+        //     name = "betty";
+        // }
+
+        string? name2 = null;
+        int? length = name2?.Length;
+        Console.WriteLine(length);
+        Console.WriteLine(name2?.Length);
+
+        name2 = "Bubby";
+        length = name2?.Length;
+        Console.WriteLine(length);
+
+        name2 ??= "Jeannie";
+        Console.WriteLine(name2);
+
+        string? name3 = null;
+        name3 ??= "Jeannie";
+        Console.WriteLine(name3);
+
+
     }
 }
