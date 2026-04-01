3c130688a47680f32995b623be08859ee228fb3a
JamesDWyatt <wya22003@byui.edu>
2026-04-01 09:36:13 -0600

    4/1/26 2DVector

---
+++ sandbox/2Dvec/2DVector.cs
@@ -0,0 +1,30 @@
+class Vector2D
+{
+    private int X;
+    private int Y;
+    public Vector2D(int x, int y)
+    {
+        X=x;
+        Y=y;
+    }
+    public string GetVector2DDisplayString()
+    {
+        return $"x: {X}, y: {Y}";
+    }
+    public static Vector2D operator +(Vector2D v1, Vector2D v2)
+    {
+        return new Vector2D(v1.X + v2.X, v1.Y + v2.Y);
+    }
+    public static Vector2D operator -(Vector2D v1, Vector2D v2)
+    {
+        return new Vector2D(v1.X - v2.X, v1.Y - v2.Y);
+    }
+    public static bool operator ==(Vector2D v1, Vector2D v2)
+    {
+        return v1.X == v2.X && v1.Y == v2.Y;
+    }
+    public static bool operator !=(Vector2D v1, Vector2D v2)
+    {
+        return v1.X == v2.X && v1.Y == v2.Y;
+    }
+}
