b3a83bcfda508a698559b8b9d5bdde5a69791c9a
JamesDWyatt <wya22003@byui.edu>
2026-04-05 22:23:46 -0600

    4/5/26 Foundation1/2 Complete

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -2,8 +2,27 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        int c = 0;
+        Video genVideo = new Video();
+        Comment genComment = new Comment();
+
+        string video = genVideo.getVideo();
+        Console.WriteLine($"{video}");
+        while (true)
+        {
+            if (c != 3)
+            {
+                string comment = genComment.getComment();
+                Console.WriteLine($"{comment}");
+                c += 1;
+            }
+            else
+            {
+                break;
+            }
+        }
     }
 }
