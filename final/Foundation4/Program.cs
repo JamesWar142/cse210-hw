2d9473b9f2637bf4b9e3be3a7b4473a0088caee7
JamesDWyatt <wya22003@byui.edu>
2026-04-07 22:09:47 -0600

    4/7/26 Foundation4 Complete

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        Activity activity= new Activity();
+        var running = new Running(activity);
+        var cycling = new Cycling(activity);
+        var swimming = new Swimming(activity);
+        Console.WriteLine("Welcome to fitness tracker. What activity would you like to do?");
+        Console.WriteLine("a. Running");
+        Console.WriteLine("b. Cycling");
+        Console.WriteLine("c. Swimming");
+        string choice=Console.ReadLine();
+        switch (choice)
+        {
+            case "a":
+                running.Date();
+                running.Time();
+                running.Distance();
+                running.Calculations();
+                running.GetSummary();
+                break;
+            case "b":
+                cycling.Date();
+                cycling.Time();
+                cycling.Distance();
+                cycling.Calculations();
+                cycling.GetSummary();
+                break;
+            case "c":
+                swimming.Date();
+                swimming.Time();
+                swimming.Distance();
+                swimming.Calculations();
+                swimming.GetSummary();
+                break;
+        }
     }
 }
